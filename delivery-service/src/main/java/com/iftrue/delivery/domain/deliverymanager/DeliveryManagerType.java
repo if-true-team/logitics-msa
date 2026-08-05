8ac8fbbc1d6ddc306385849933fc44ad5ddf84b3
taeeonkim <myhom43@gmail.com>
2026-08-05 16:30:41 +0900

    feat: 배송 관련 enum 클래스 추가 (HubDeliveryStatus, DeliveryManagerType)

---
+++ delivery-service/src/main/java/com/iftrue/delivery/domain/deliverymanager/DeliveryManagerType.java
@@ -0,0 +1,5 @@
+package com.iftrue.delivery.domain.deliverymanager;
+
+public enum DeliveryManagerType {
+    HUB, COMPANY
+}
---
+++ delivery-service/src/main/java/com/iftrue/delivery/domain/deliveryroute/HubDeliveryStatus.java
@@ -0,0 +1,7 @@
+package com.iftrue.delivery.domain.deliveryroute;
+
+public enum HubDeliveryStatus {
+    WAITING_FOR_DEPARTURE,
+    IN_TRANSIT,
+    ARRIVED,
+}
