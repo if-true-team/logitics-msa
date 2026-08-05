fd65effebd5de00c3c43b2715baed6209905f57f
choin <choin0330@gmail.com>
2026-08-05 11:09:11 +0900

    feat: 허브 도메인 엔티티 및 레포지토리 추가 (#8)

---
+++ hub-service/src/main/java/com/iftrue/hub/domain/HubRouteRepository.java
@@ -0,0 +1,8 @@
+package com.iftrue.hub.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface HubRouteRepository extends JpaRepository<HubRoute, UUID> {
+}
