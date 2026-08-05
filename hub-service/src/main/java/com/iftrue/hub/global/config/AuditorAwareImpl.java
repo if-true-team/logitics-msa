9db992e3560c78c0c8b770008adeb6f96b5ce4f5
choin <choin0330@gmail.com>
2026-08-05 21:00:44 +0900

    feat: JPA Auditing 설정 및 임시용 유저 정보 스텁 추가 (#13)

---
+++ hub-service/src/main/java/com/iftrue/hub/global/config/AuditorAwareImpl.java
@@ -0,0 +1,21 @@
+package com.iftrue.hub.global.config;
+
+import com.iftrue.hub.global.security.CurrentUserProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AuditorAwareImpl implements AuditorAware<UUID> {
+
+    private final CurrentUserProvider currentUserProvider;
+
+    @Override
+    public Optional<UUID> getCurrentAuditor() {
+        return Optional.of(currentUserProvider.getCurrentUserId());
+    }
+}
