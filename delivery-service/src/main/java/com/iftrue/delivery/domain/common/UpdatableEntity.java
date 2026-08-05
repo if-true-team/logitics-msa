1758ecdc8a8f7f0bf17c7b17eb5972627531adde
taeeonkim <myhom43@gmail.com>
2026-08-05 16:28:16 +0900

    feat: 공통 엔티티(BaseEntity, UpdatableEntity, DeletableEntity) 추가

---
+++ delivery-service/src/main/java/com/iftrue/delivery/domain/common/UpdatableEntity.java
@@ -0,0 +1,22 @@
+package com.iftrue.delivery.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@Getter
+public abstract class UpdatableEntity extends BaseEntity {
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", nullable = false)
+    private String updatedBy;
+}
